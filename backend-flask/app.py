7ad53668998ea5c22ffe59f3ca87fefe4ad8494d
dapetoo <dapetoo@gmail.com>
2023-03-06 13:08:45 +0000

    AWS Xray

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -29,6 +29,14 @@ provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
+#AWS X-Ray SDK
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
+
 
 app = Flask(__name__)
 
