b58d6e1da675d593f948f1764bb7f644504bf833
Peter Dada <dapetoo@gmail.com>
2023-03-09 16:12:57 +0100

    Instrumentation

--- frontend-react-js/src/tracing.js
+++ frontend-react-js/src/tracing.js
@@ -1,44 +1,66 @@
-import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
-import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+// tracing.js
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
-import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { WebTracerProvider, BatchSpanProcessor } from '@opentelemetry/sdk-trace-web';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
-const { Resource } = require('@opentelemetry/resources');
-const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+import { Resource }  from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
+import { UserInteractionInstrumentation } from '@opentelemetry/instrumentation-user-interaction';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
 
 const exporter = new OTLPTraceExporter({
- url: 'https://<your collector endpoint>:443/v1/traces'
+  url: 'https://api.honeycomb.io/v1/traces'
 });
 const provider = new WebTracerProvider({
- resource: new Resource({
-
-   [SemanticResourceAttributes.SERVICE_NAME]: 'browser',
- }),
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'browser',
+  }),
 });
 provider.addSpanProcessor(new BatchSpanProcessor(exporter));
 provider.register({
- contextManager: new ZoneContextManager()
+  contextManager: new ZoneContextManager()
+});
+
+provider.addSpanProcessor(new BatchSpanProcessor(exporter));
+provider.register({
+  contextManager: new ZoneContextManager()
 });
 
 registerInstrumentations({
- instrumentations: [
-   getWebAutoInstrumentations({
-     // load custom configuration for xml-http-request instrumentation
-     '@opentelemetry/instrumentation-xml-http-request': {
-       propagateTraceHeaderCorsUrls: [
-           /.+/g,
-         ],
-     },
-     // load custom configuration for fetch instrumentation
-     '@opentelemetry/instrumentation-fetch': {
-       propagateTraceHeaderCorsUrls: [
-           /.+/g,
-         ],
-     },
-   }),
- ],
+  instrumentations: [
+    new XMLHttpRequestInstrumentation({
+      propagateTraceHeaderCorsUrls: [
+         /.+/g, //Regex to match your backend urls. This should be updated.
+      ]
+    }),
+    new FetchInstrumentation({
+      propagateTraceHeaderCorsUrls: [
+         /.+/g, //Regex to match your backend urls. This should be updated.
+      ]
+    }),
+  ],
 });
+
+registerInstrumentations({
+  instrumentations: [
+    new DocumentLoadInstrumentation(),
+  ],
+});
+
+registerInstrumentations({
+  instrumentations: [
+    new UserInteractionInstrumentation(),
+  ],
+});
+
+registerInstrumentations({
+  instrumentations: [
+    new UserInteractionInstrumentation({
+      eventNames: ['submit', 'click', 'keypress'],
+    }),
+  ],
+});
+
 
 export default function TraceProvider({ children }) {
   return (
@@ -47,30 +69,3 @@ export default function TraceProvider({ children }) {
     </>
   );
 }
-// // tracing.js for instrumenting frontend 
-// import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-// import { WebTracerProvider, BatchSpanProcessor } from '@opentelemetry/sdk-trace-web';
-// import { ZoneContextManager } from '@opentelemetry/context-zone';
-// import { Resource }  from '@opentelemetry/resources';
-// import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-
-// const exporter = new OTLPTraceExporter({
-//   url: 'https://api.honeycomb.io/v1/traces'
-// });
-// const provider = new WebTracerProvider({
-//   resource: new Resource({
-//     [SemanticResourceAttributes.SERVICE_NAME]: 'crudder-browser',
-//   }),
-// });
-// provider.addSpanProcessor(new BatchSpanProcessor(exporter));
-// provider.register({
-//   contextManager: new ZoneContextManager()
-// });
-
-// export default function TraceProvider({ children }) {
-//   return (
-//     <>
-//       {children}
-//     </>
-//   );
-// }
